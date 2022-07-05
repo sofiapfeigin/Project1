5c0b08a3e5348bfbc85189ed70e6ea7b4e8dede9
sofiapfeigin <sofiapfeigin@gmail.com>
2022-07-05 12:24:10 -0300

    tercer commit

--- src/main/Proyect1.java
+++ src/main/Proyect1.java
@@ -1,22 +1,28 @@
 package main;
 
-
 import java.util.Scanner;
 
 public class Proyect1 {
-    public static void main(String[] args) {
-    	
-    	
-    	Scanner in = new Scanner(System.in);
-    	String palabra,palabraNueva="";
-    	System.out.println("Ingrese una palabra");
-    	palabra = in.nextLine();
-    	//palabraNueva=palabra.substring(1,palabra.length())+palabra.substring(0,1)+"ay";
-    	String [] palabras=palabra.split(" ");
-    	for(int i=0; i<palabras.length;i++)
-    	{
-    		palabraNueva=palabraNueva+palabras[i].substring(1,palabras[i].length())+palabras[i].substring(0,1)+"ay ";
-    	}
-    	System.out.println("La palabra nueva es: "+palabraNueva);
-    } 
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		String palabra, palabraNueva = "";
+		System.out.println("Ingrese una palabra");
+		palabra = in.nextLine();
+		// palabraNueva=palabra.substring(1,palabra.length())+palabra.substring(0,1)+"ay"; // //Ejercicio 1
+		String[] palabras = palabra.split(" ");
+		for (int i = 0; i < palabras.length; i++) {
+			// palabraNueva=palabraNueva+palabras[i].substring(1,palabras[i].length())+palabras[i].substring(0,1)+"ay // "; //Ejercicio 2
+			palabraNueva = palabraNueva + palabraFinal(palabras[i]);
+		}
+		System.out.println("La palabra nueva es: " + palabraNueva);
+	}
+
+	//Ejercicio 3
+	public static String palabraFinal(String aux) {
+		String primeraLetra = aux.substring(1, 2);
+		aux = primeraLetra.toUpperCase() + aux.substring(2, aux.length()) + aux.substring(0, 1) + "ay ";
+		return aux;
+	}
+
 }
